77ce364b7f3b3047d7c8ead12e067e84d27c09b0
IrshadAM <irshad.mallick@hotmail.com>
2014-11-29 20:45:04 +0300

    jsFiddle tidy-up

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-$(document).ready(function(){
-  $("#button").click(function(){
+$(document).ready(function () {
+  $("#button").click(function () {
     var toAdd = $("input[name=checkListItem]").val();
     $("#remove-instruct").remove();
     $(".list").append('<div class="item">' + toAdd + '</div>');
   });
-  $(document).on("click", ".item", function(){
+  $(document).on("click", ".item", function () {
     $(this).addClass("strikeout");
   });
-  $(document).on("dblclick", ".item", function(){
+  $(document).on("dblclick", ".item", function () {
     $(this).remove();
   });
 });
